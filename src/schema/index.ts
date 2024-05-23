e02d05f1fd1fd5f27a5a476e98b15adeed63f208
Hemant <hemantjadhav8825@gmail.com>
2024-05-24 00:02:53 +0530

    query demo added in schema

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,3 +1,33 @@
-const { GraphQLSchema } = require("graphql");
+const { GraphQLSchema, GraphQLObjectType, GraphQLString } = require("graphql");
 
-module.exports = new GraphQLSchema({});
+const productType = new GraphQLObjectType({
+  name: "Product",
+  fields: () => ({
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+  }),
+});
+
+const rootQuery = new GraphQLObjectType({
+  name: "RootQuery",
+  fields: {
+    Product: {
+      type: productType,
+      resolve: (parent: any, args: any) => {
+        return {
+          id: "demo",
+          name: "Bupendra jogi",
+        };
+      },
+    },
+  },
+});
+
+const rootMutation = new GraphQLObjectType({
+  name: "RootMutation",
+});
+
+module.exports = new GraphQLSchema({
+  query: rootQuery,
+  //   mutation: rootMutation,
+});
